8ea231a81060b476c4240d1db71809608cd373af
Juan David Pastas Rivera <juandavid1707@gmail.com>
2014-07-14 15:49:04 -0500

    Initial commit.

---
+++ b.js
@@ -0,0 +1,5 @@
+module.exports = {
+  some: function(){
+    console.log('qweqweqwe')
+  }
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+var express    = require('express');
+var flash      = require('connect-flash');
+var session    = require('express-session');
+var livereload = require('express-livereload');
+var engines    = require('consolidate');
+
+var app = express();
+app.use(session({secret: 'keyboard cat', saveUninitialized: true,
+                 resave: true}))
+app.use(flash());
+
+livereload(app);
+
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+app.get('/', function(req, res){
+
+  console.log('asdq')
+  res.render('index.html', {flash: req.flash()});
+});
+
+app.route('/:path').
+  all(function(req, res){
+  req.flash('path', req.param('path'));
+  res.redirect('/');
+});
+
+app.listen(3000);
+console.log("Started!")
