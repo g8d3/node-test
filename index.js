785de7e76f956a4e64f6f86284753727faf7fdc6
Juan David Pastás Rivera <juandavid1707@gmail.com>
2014-07-16 13:35:04 -0500

    WIP. Debug with node-inspector and nodemon.

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
+var debug      = require('debug')('myapp');
 var express    = require('express');
-var flash      = require('connect-flash');
 var session    = require('express-session');
-var livereload = require('express-livereload');
 var engines    = require('consolidate');
+debug('with colors');
 
-livereload(app);
 var app = express();
 app.use(session({secret: 'keyboard cat', saveUninitialized: true,
                  resave: true}))
-app.use(flash());
 
 
 app.set('views', __dirname + '/views');
 app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 
-app.get('/', function(req, res){
-  res.render('index.html', {flash: req.flash()});
-});
-
-app.route('/:path').
-  all(function(req, res){
-  req.flash('path', req.param('path'));
-  res.redirect('/');
+app.get('*',function(req, res){
+  require('fs').readFile('views/index.html', 'utf8', function(err, data){
+    if(err) throw err;
+    res.send(data);
+  })
+  // res.sendfile('views/index.html');
 });
 
 app.listen(3000);
-console.log("Started!")
+debug("Started!");
